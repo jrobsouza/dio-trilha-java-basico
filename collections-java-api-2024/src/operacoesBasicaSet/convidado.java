f23fe1f86f6c25b88fa1091b697856c51caf7cc2
Jose Roberto <jrobsouza@hotmail.com>
2024-10-12 11:06:04 -0300

    realizando update das pastas e arquivos

---
+++ collections-java-api-2024/src/operacoesBasicaSet/convidado.java
@@ -0,0 +1,30 @@
+package operacoesBasicaSet;
+
+public class convidado {
+    //atributos do convidado
+    private String nome;
+    private int codigoConvite;
+
+    public convidado( String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+
+        return codigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
