9f961978b275025fca6b7fc799b71a1190bb2505
Jose Roberto <jrobsouza@hotmail.com>
2024-10-22 18:25:24 -0300

    exercicios de Stream e exemplos de como utilizar na pratica

---
+++ StreamAPI/src/Desafios/stream/mostrarOrdemnum.java
@@ -0,0 +1,19 @@
+package Desafios.stream;
+
+import java.util.Arrays;
+import java.util.List;
+//Desafio 1 - Mostre a lista na ordem numérica:
+//Crie um programa que utilize a Stream API para ordenar a lista de números em ordem crescente e a exiba no console.
+public class mostrarOrdemnum {
+    private static Object List;
+
+    public static void main (String [] args) {
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+         (List < Integer > numeros) {
+        this.numeros = numeros;
+    }
+
+}
+
+}
